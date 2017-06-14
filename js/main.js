367a99a7834bb8b2c5141558139b9b85155fbf01
Tyler Clark <TheTDrive@Tylers-MacBook-Pro.local>
2017-06-14 17:26:26 -0600

    created matched pair function and cardMatch array. Makes checkForMatch function work in multiple instances

--- js/main.js
+++ js/main.js
@@ -24,19 +24,29 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var correctMatches = [];
 var score = 0;
+var scoreboard = document.getElementById('scoreboard');
 
+var clearScore = function() {
+	score = 0;
+	scoreboard.innerHTML = 'Your Score ' + score;
+};
 var updateScore = function() {
-	var scoreboard = document.getElementById('scoreboard');
 	score += 1
 	scoreboard.innerHTML = 'Your Score: ' + score;
+};
+var matchedPair = function() {
+	var match = cardsInPlay.splice(0, 2);
+	correctMatches.push(match);
+	updateScore();
 };
 
 var checkForMatch = function() {
 	if(cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match");
-			updateScore();
+			matchedPair();
 			} else {
 			alert("Sorry, try again.");
 			createBoard();
@@ -55,6 +65,7 @@ var createBoard = function() {
 	var gameBoard = document.getElementById('game-board');
 	gameBoard.innerHTML = '';
 	cardsInPlay = [];
+	clearScore();
 	for(var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', '../wdi-fundamentals-memorygame/images/back.png');
